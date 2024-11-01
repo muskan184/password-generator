7e5612f8370471b8807afbd464cbca78088d78de
Muskan Dubey <muskandubey184@outlook.com>
2024-11-01 23:34:33 +0530

    Update header.jsx

--- src/header.jsx
+++ src/header.jsx
@@ -1,6 +1,21 @@
+// import "./App.css";
+// function Header() {
+//   return <h1>this is header from another file</h1>;
+// }
+
+// export default Header;
 import "./App.css";
-function Header() {
-  return <h1>this is header from another file</h1>;
+function Header(props, children) {
+  let { hraderinfo } = props;
+  return (
+    <div>
+      {props.children}
+      <h1>
+        name is {hraderinfo.name} <br />
+        and phone number is {hraderinfo.phone}
+      </h1>
+    </div>
+  );
 }
 
 export default Header;
